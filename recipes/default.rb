85a9d345746fb4dc12de14df4e7fad11d9d644af
Cody Dracars <cdracars@gmail.com>
2013-04-03 12:52:43 -0500

    Fix for FC039

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,18 +1,18 @@
-dir = node["keys"]["path"]
+dir = node.keys["path"]
 
 directory dir do
-  owner node["keys"]["user"]
-  group node["keys"]["group"]
+  owner node.keys["user"]
+  group node.keys["group"]
 
   action :create
 end
 
-data_bag_item( node["keys"]["databag"], node["keys"]["key_name"] ).each do |k, v|
+data_bag_item( node.keys["databag"], node.keys["key_name"] ).each do |k, v|
   next unless k.match(/^id_/)
 
   file "#{dir}/#{k}" do
-    owner node["keys"]["user"]
-    group node["keys"]["group"]
+    owner node.keys["user"]
+    group node.keys["group"]
     mode k.match(/\.pub$/) ? "644" : "600"
     content v
   end
